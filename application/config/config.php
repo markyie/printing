c00873926f1f914205b91bb060c71ab4d5fa292e
Sumri Mat Yajid <sumri7@gmail.com>
2018-03-22 00:10:21 +0800

    add HARVIACODE

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://localhost/printing/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -308,7 +308,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'UknlcKiPnJMrLNdYkluQf0mDtBCsAXCz';
 
 /*
 |--------------------------------------------------------------------------
